fe3d05f4657983ed833f2784dd703c193f840639
barathykolappan <barathykolappan@gmail.com>
2019-03-23 17:35:39 +0530

    v1.6

--- Index.py
+++ Index.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+import re
 app=Flask(__name__)
 @app.route('/')
 def index():
@@ -7,9 +10,49 @@ def index():
 def getvalue():
     cname=request.form['cname']
     uname=request.form['uname']
-    coname=request.form['coname']
-    print(cname,uname,coname)
-    #return render_template("result.html")
+    #coname=request.form['coname']
+    quote_page = "https://www.timeshighereducation.com/world-university-rankings/"+uname.replace(" ","-")
+    page = requests.get(quote_page)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    name= soup.find('div',class_="institution-info__contact-detail institution-info__contact-detail--address")
+    rank=soup.find('div',class_="rank__number")
+    n=re.sub('<[^>]+>', '',str(name)).strip()
+    last_n=n.rfind(',')
+    n=str(n[int(last_n+1):])
+    r=re.sub('<[^>]+>', '',str(rank))
+    if cname in n:
+        #print(n)
+        rank=r
+        if rank is "o":
+            rank="NA"
+        rank=rank.replace("th","")
+        rank=rank.replace("rd","") 
+        rank=rank.replace("st","")
+        return render_template("result.html",rank=rank,uname=uname)
+    else:
+        rank=rank.replace("th","")
+        rank=rank.replace("rd","") 
+        rank=rank.replace("st","")
+        return render_template("cverify.html",cname=n,uname=uname)
+@app.route('/<value>'   )
+def getvaluere(value):
+    uname=value
+    quote_page = "https://www.timeshighereducation.com/world-university-rankings/"+uname.replace(" ","-")
+    page = requests.get(quote_page)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    name= soup.find('div',class_="institution-info__contact-detail institution-info__contact-detail--address")
+    rank=soup.find('div',class_="rank__number")
+    n=re.sub('<[^>]+>', '',str(name)).strip()
+    last_n=n.rfind(',')
+    n=str(n[int(last_n+1):])
+    rank=re.sub('<[^>]+>', '',str(rank))
+    if rank is "o":
+        rank="NA"
+    rank=rank.replace("th","")
+    rank=rank.replace("rd","") 
+    rank=rank.replace("st","")
+    return render_template("result.html",rank=rank,uname=uname)
+        
 if __name__=='__main__':
     app.run()
     
